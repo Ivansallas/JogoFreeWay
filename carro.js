6da8593cec5707bf7af9a2b99f9c66f90617d7a0
Ivansallas <Kiss_ivan@hotmail.com>
2026-04-30 12:05:51 -0300

    ok

--- carro.js
+++ carro.js
@@ -4,10 +4,27 @@ let xCarro = 600;
 let larguraCarro = 100;
 let alturaCarro = 50;
 
+  
+//carro
+let yCarro2 = 160;
+let xCarro2 = 600;
+let larguraCarro2 = 100;
+let alturaCarro2 = 50;
+
+//carro
+let yCarro3 = 260;
+let xCarro3 = 600;
+let larguraCarro3 = 100;
+let alturaCarro3 = 50;
+
 function mostraCarro() {
   image(imagemCarro, xCarro, yCarro, larguraCarro, alturaCarro);
+  image(imagemCarro2, xCarro2, yCarro2, larguraCarro2, alturaCarro2);
+  image(imagemCarro3, xCarro3, yCarro3, larguraCarro3, alturaCarro3);
 }
 
 function movimentaCarro() {
   xCarro -= 2;
+  xCarro2 -= 2;
+  xCarro3 -= 2;
 }
--- imagens.js
+++ imagens.js
@@ -1,9 +1,13 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
 
 function preload() {
   imagemDaEstrada = loadImage("imagens/estrada.png");
   imagemDoAtor = loadImage("imagens/boneconeve.png");
   imagemCarro = loadImage("imagens/carro1.png");
+  imagemCarro2 = loadImage("imagens/carro2.png");
+  imagemCarro3 = loadImage("imagens/carro3.png");
 }
